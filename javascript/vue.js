f3d88b82c16142159c217730922a4d31b6421db2
Wika Silo <wikanyaa@gmail.com>
2019-06-20 12:58:11 +0700

    resolve indentasi

--- javascript/vue.js
+++ javascript/vue.js
@@ -2,23 +2,23 @@ const app = new Vue({
   el: '#app',
   data: {
     todos: [{
-        "id": 1,
-        "title": "belajar javascript",
-        "created": "naruto",
-        "image": "https://vignette.wikia.nocookie.net/naruto/images/0/09/Naruto_newshot.png/revision/latest?cb=20170621101134",
-        "check": false
-      }, {
-        "id": 2,
-        "title": "master vue",
-        "created": "sasuke",
-        "image": "https://vignette.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png/revision/latest?cb=20170621055519&path-prefix=id",
-        "check": false
-      }, {
-        "id": 3,
-        "title": "masak",
-        "created": "sakura",
-        "image": "https://vignette.wikia.nocookie.net/naruto/images/6/64/Sakura_Part_1.png/revision/latest?cb=20170726101444",
-        "check": true
+      "id": 1,
+      "title": "belajar javascript",
+      "created": "naruto",
+      "image": "https://vignette.wikia.nocookie.net/naruto/images/0/09/Naruto_newshot.png/revision/latest?cb=20170621101134",
+      "check": false
+    }, {
+      "id": 2,
+      "title": "master vue",
+      "created": "sasuke",
+      "image": "https://vignette.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png/revision/latest?cb=20170621055519&path-prefix=id",
+      "check": false
+    }, {
+      "id": 3,
+      "title": "masak",
+      "created": "sakura",
+      "image": "https://vignette.wikia.nocookie.net/naruto/images/6/64/Sakura_Part_1.png/revision/latest?cb=20170726101444",
+      "check": true
     }],
     newTodo: {
       title: '',
@@ -36,9 +36,14 @@ const app = new Vue({
     }
   },
   watch: {
-    question: function(newQuestion, oldQuestion) {
-      this.answer = 'loading...'
-      this.getAnswerApi()
+    question: function (newQuestion, oldQuestion) {
+      if (this.question.length !== 0) {
+        this.answer = 'loading...'
+        this.getAnswerApi()
+      } else {
+        this.answer = ''
+        this.gif = ''
+      }
     }
   },
   methods: {
@@ -51,51 +56,18 @@ const app = new Vue({
         check: false
       }
       //! HIT DATABASE CREATE
-      // axios({
-      //   method: 'post',
-      //   url: 'http://localhost:3000/todos',
-      //   data: this.newTodo
-      // })
-      //   .then(response => {
-      //     const { data } = response
-      //     this.todos.push(data)
-      //     this.newTodo = {
-      //       title: '',
-      //       image: '',
-      //       created: '',
-      //       check: false
-      //     }
-      //   })
-      //   .catch(err => {
-      //     console.log(err)
-      //   })
     },
     deleteTodo(input) {
       let filter = this.todos.filter(element => {
-        if(element.id !== input) {
+        if (element.id !== input) {
           return element
         }
       })
       //! HIT DATABASE DELETE
-      // axios({
-      //   method: 'delete',
-      //   url: `http://localhost:3000/todos/${input}`,
-      // })
-      //   .then(response => {
-      //     let filter = this.todos.filter(element => {
-      //       if(element.id !== input) {
-      //         return element
-      //       }
-      //     })
-      //     this.todos = filter
-      //   })
-      //   .catch(err => {
-      //     console.log(err)
-      //   })
     },
     updateTodo(input) {
       this.todos = this.todos.map(element => {
-        if(element.id === input) {
+        if (element.id === input) {
           element.check = !element.check
         }
         return element
@@ -116,18 +88,4 @@ const app = new Vue({
     }
   },
   // ! RENDER CREATED
-  // created: function() {
-  //   // fetch data
-  //   axios({
-  //     method: 'get',
-  //     url: 'http://localhost:3000/todos'
-  //   })
-  //     .then(response => {
-  //       const { data } = response
-  //       this.todos = data
-  //     })
-  //     .catch(err => {
-  //       console.log(err)
-  //     })
-  // }
 })
